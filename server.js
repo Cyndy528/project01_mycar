3d346d1c1e987d3c8d4524fd58ab1c6ab832113b
DreTGiant <amassey30@yahoo.com>
2015-11-27 13:50:02 -0800

    sign up and login done

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 var express = require('express'),
-  app = express();
+	app = express();
+// require and load dotenv
+require('dotenv').load();
 
-  var exhbs = require('hbs');
-   app.set('view engine', 'hbs');
+var exhbs = require('hbs');
+app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/public'));
 
 
-
-
-
-
-
-
-
-  // Homepage route
-app.get('/', function(req, res) {
-  res.render('index');
+// Sign Up route
+app.get('/login', function(req, res) {
+	res.render('login');
+});
+// Sign Up route
+app.get('/sign_up', function(req, res) {
+	res.render('sign_up');
+});
+// Homepage route
+app.get('/index', function(req, res) {
+	res.render('index');
 });
 
 var server = app.listen(process.env.PORT || 3000, function() {
-  console.log('Hello...Is it me your looking for..')
+	console.log('Hello...Is it me your looking for..')
 });
